7fa74262b130d7c69a982508197ec3ab3acc0bdb
justinvyu <justin.v.yu@gmail.com>
2019-03-24 16:53:24 -0700

    Add VPG learning curves

---
+++ VPG/plot.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+import numpy as np
+import os
+
+env = "CartPole-v0"
+data_path = "./train_history/" + env + ".pkl"
+
+with open(data_path, "rb") as f:
+    data = pickle.load(f)
+
+sns.set(style="darkgrid")
+
+return_means = data["return_means"]
+return_stds = data["return_stds"]
+batch_size = data["batch_size"]
+lr = data["lr"]
+model = data["policy_layer_representation"]
+epochs = len(return_means)
+
+print(epochs, return_means, return_stds, model)
+
+plt.errorbar(range(epochs),
+             return_means,
+             yerr=return_stds,
+             capsize=2, elinewidth=1, markeredgewidth=1)
+
+plt.suptitle("VPG Learning Curve on environment: " + env, y=0.99)
+plt.title("(Params) batch_size={0}, lr={1}, epochs={2}".format(batch_size, lr, epochs)
+          + "\n(Error bars: std of returns sampled during each epoch)", fontsize=10)
+plt.xlabel("Epoch")
+plt.ylabel("Mean of Returns")
+plt.legend(["Trained Policy"])
+plt.show()
--- VPG/vpg.py
+++ VPG/vpg.py
@@ -5,6 +5,8 @@ from torch.distributions import Categorical
 import torch.optim as optim
 import numpy as np
 import gym
+import pickle
+import os
 
 class MLP(nn.Module):
     def __init__(self, input_size, layer_sizes, output_size, activation=F.relu, output_activation=None):
@@ -13,7 +15,7 @@ class MLP(nn.Module):
         self.activation = activation
         self.output_activation = output_activation
         layers = nn.ModuleList()
-        print(input_size, layer_sizes, output_size)
+        # print(input_size, layer_sizes, output_size)
         layers.append(nn.Linear(input_size, layer_sizes[0]))
         for i in range(len(layer_sizes) - 1):
             layers.append(nn.Linear(layer_sizes[i], layer_sizes[i + 1]))
@@ -35,6 +37,7 @@ class CategoricalPolicy(nn.Module):
 
         # Represent the function that calculates action logits as an MLP.
         self.logits = MLP(observation_dim, hidden_layers, action_dim, activation=activation)
+        self.model_representation = [observation_dim] + hidden_layers + [action_dim]
 
     def forward(self, x, action=None):
         observation = x
@@ -53,6 +56,7 @@ class CategoricalPolicy(nn.Module):
 
 class VPG:
     def __init__(self, env):
+        self.env_name = env
         self.env = gym.make(env)
         self.observation_dim = self.env.observation_space.shape[0]
         self.action_dim = self.env.action_space.n
@@ -61,18 +65,47 @@ class VPG:
     def build_model(self, hidden_layers=[32]):
         return CategoricalPolicy(self.observation_dim, hidden_layers, self.action_dim)
 
-    def train(self, batch_size=5000, epochs=50, lr=1e-2):
-        """
+    def save_checkpoint(self):
+        torch.save(self.policy.state_dict(), "./weights/" + self.env_name)
 
-        :param batch_size:
-        :param epochs:
-        :param lr:
-        :return:
-        """
+    def load_checkpoint(self):
+        self.policy.load_state_dict(torch.load("./weights/" + self.env_name))
+
+    def train(self, batch_size=5000, epochs=100, lr=1e-2):
+        return_means, return_stds = [], []
         for epoch in range(epochs):
-            self.train_step(batch_size, lr)
+            loss, returns, lens = self.train_step(batch_size, lr)
+            print('epoch: %3d \t loss: %.3f \t avg return: %.3f \t ep_len: %.3f' %
+                  (epoch, loss, np.mean(returns), np.mean(lens)))
+            return_means.append(np.mean(returns))
+            return_stds.append(np.std(returns))
+            self.save_checkpoint()
 
-    def train_step(self, batch_size, lr=1e-2):
+        with open(os.path.join('train_history', self.env_name + '.pkl'), 'wb') as f:
+            pickle.dump({"env": self.env_name,
+                         "return_means": np.array(return_means),
+                         "return_stds": np.array(return_stds),
+                         "batch_size": batch_size,
+                         "lr": lr,
+                         "policy_layer_representation": self.policy.model_representation},
+                        f, pickle.HIGHEST_PROTOCOL)
+
+    def reward_to_go(self, rewards):
+        """
+        Calculates the total reward collected from the current time-step t
+        until the end of the episode. Reduces variance, using the intuition that
+        present cannot affect the past.
+
+        >>> list(VPG.reward_to_go(None, [-1, 0, 1, 2, 3]))
+        [5.0, 6.0, 6.0, 5.0, 3.0]
+        """
+        n = len(rewards)
+        rewards_to_go = np.zeros(n)
+        for i in reversed(range(n)):
+            rewards_to_go[i] = rewards[i] + (rewards_to_go[i + 1] if i + 1 < n else 0)
+        return rewards_to_go
+
+    def train_step(self, batch_size, lr=1e-2, render=False):
         """
         Performs one epoch of training, which consists of two parts:
         1. Experience Collection
@@ -93,6 +126,9 @@ class VPG:
         batch_rewards = []
         ep_rewards = []
 
+        batch_returns = []
+        batch_lens = []
+
         observation = env.reset()
         while True:
             batch_observations.append(observation.copy())
@@ -102,16 +138,19 @@ class VPG:
             observation, reward, done, _ = env.step(action.numpy())
             batch_actions.append(action)
             ep_rewards.append(reward)
-            env.render()
+            if render:
+                env.render()
 
             # Handle what happens when the episode is over.
             if done:
                 total_reward, num_steps = sum(ep_rewards), len(ep_rewards)
+                batch_returns.append(total_reward)
+                batch_lens.append(num_steps)
+
                 # Each step in this trajectory is associated with the end reward.
-                batch_rewards += [total_reward] * num_steps
+                batch_rewards += list(self.reward_to_go(ep_rewards))
 
                 observation, ep_rewards, done = env.reset(), [], False
-
                 if len(batch_observations) > batch_size:
                     break
 
@@ -122,10 +161,44 @@ class VPG:
 
         # Define loss function.
         surrogate_loss = -(log_probs * advantages).mean()
+        # Take one step of gradient descent.
         optimizer.zero_grad()
         surrogate_loss.backward()
         optimizer.step()
 
+        return surrogate_loss, batch_returns, batch_lens
+
+    def evaluate(self, num_rollouts=25, render=False):
+        env = self.env
+
+        batch_returns = []
+        batch_lens = []
+        for i in range(num_rollouts):
+            observation = env.reset()
+            ep_rewards = []
+            done = False
+            while not done:
+                action, _ = self.policy(torch.Tensor(observation))
+                observation, reward, done, _ = env.step(action.numpy())
+                ep_rewards.append(reward)
+                if render:
+                    env.render()
+
+            ep_return, ep_len = sum(ep_rewards), len(ep_rewards)
+            batch_returns.append(ep_return)
+            batch_lens.append(ep_len)
+
+            print('rollout #: %3d \t return: %.3f \t ep_len: %.3f' %
+                  (i, ep_return, ep_len))
+
+        print('avg return: %.3f \t avg ep_len: %.3f' %
+              (np.mean(batch_returns), np.mean(batch_lens)))
+
 if __name__ == "__main__":
     vpg = VPG("CartPole-v0")
-    vpg.train()
+    train = True
+    if train:
+        vpg.train()
+    else:
+        vpg.load_checkpoint()
+        vpg.evaluate()
